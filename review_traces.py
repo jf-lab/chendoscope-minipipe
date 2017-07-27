9c4595bca0384d9dbccc67aed4e6da709ff495a8
miniscope <andrew@ca-imaging.sickkids.ca>
2017-07-27 12:23:07 -0400

    GUI for reviewing traces

---
+++ review_traces.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+
+def review_traces(trace_array):
+
+    traces_to_keep = []
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.2)
+    t = np.arange(0, trace_array.shape[1])
+    trace, = plt.plot(t, trace_array[0, :])
+
+    class Index(object):
+        ind = 0
+
+        def keep(self, event):
+            traces_to_keep.append(self.ind)
+            self.ind += 1
+            if self.ind >= trace_array.shape[0]:
+                plt.close()
+            else:
+                ydata = trace_array[self.ind, :]
+                trace.set_ydata(ydata)
+                plt.draw()
+
+        def exclude(self, event):
+            self.ind += 1
+            if self.ind >= trace_array.shape[0]:
+                plt.close()
+            else:
+                ydata = trace_array[self.ind, :]
+                trace.set_ydata(ydata)
+                plt.draw()
+
+
+    callback = Index()
+    axkeep = plt.axes([0.81, 0.05, 0.1, 0.075])
+    axexclude = plt.axes([0.7, 0.05, 0.1, 0.075])
+    bkeep = Button(axkeep, 'Keep')
+    bkeep.on_clicked(callback.keep)
+    bexclude = Button(axexclude, 'Exclude')
+    bexclude.on_clicked(callback.exclude)
+    plt.show()
+
+    return traces_to_keep
