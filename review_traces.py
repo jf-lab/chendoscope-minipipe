ebe67a7ff934bba69a2eb108eabb00466b28aa65
miniscope <lina.mntran@gmail.com>
2017-11-02 10:51:25 -0400

    Added hotkeys, undo, checkpointing

--- review_traces.py
+++ review_traces.py
@@ -23,12 +23,19 @@ from scipy.sparse import issparse
 def get_args():
     parser = argparse.ArgumentParser(description='Review CNMF-E traces')
     parser.add_argument('input', help='.mat output files to review')
+    parser.add_argument('-f', '--fs', help='Sampling rate for CNMFE input')
+    parser.set_defaults(fs=5)
+    parser.add_argument('-c', '--checkpoint', help='Whether to pick up from checkpoint', action='store_true')
     return parser.parse_args()
 
 
-def review_traces(data):
+def review_traces(data, fs, checkpoint_ind, keep):
 
-    traces_to_keep = []
+    ind_c = checkpoint_ind
+    traces_to_keep = keep
+    print(ind_c)
+
+    check_ind = 0
     ds = data['ssub']
 
     fig = plt.figure()
@@ -40,20 +47,20 @@ def review_traces(data):
     ax.matshow(data['A'][:,0].reshape(int(648/ds), int(486/ds)))
 
     # raw and fitted traces
-    t = np.arange(0, data['C'].shape[1])
+    t = np.arange(0, data['C'].shape[1])/fs[0,:]
     ax1 = fig.add_subplot(grid[:2, 2:] )
-    trace_Craw, = ax1.plot(t, data['C_raw'][0, :])
+    trace_Craw, = ax1.plot(t, data['C_raw'][ind_c, :])
     ax2 = fig.add_subplot(grid[2:,2:], sharex=ax1)
-    trace_C, = ax2.plot(t, data['C'][0, :])
+    trace_C, = ax2.plot(t, data['C'][ind_c, :])
 
     plt.setp(ax1, title='Raw Trace')
     plt.setp(ax2, title='Fitted Trace')
     grid.update(top=0.4, left=0.5, hspace=0.4, wspace=0.2)
     grid.tight_layout(fig)
-    fig.suptitle("Cell 0 of {}".format(data['C'].shape[0]), x=0.1)
+    fig.suptitle("Cell {} of {}".format(ind_c, data['C'].shape[0]), x=0.1)
 
     class Index(object):
-        ind = 0
+        ind = ind_c
 
         def keep(self, event):
             traces_to_keep.append(self.ind)
@@ -83,16 +90,44 @@ def review_traces(data):
                 fig.suptitle("Cell {} of {}".format(self.ind, data['C'].shape[0]), x=0.1)
                 plt.draw()
 
+        def undo(self, event):
+            self.ind -= 1
+            del traces_to_keep[-1]
+            ax.matshow(data['A'][:,self.ind].reshape(int(648/ds), int(486/ds)))
+            ydata = data['C'][self.ind, :]
+            trace_C.set_ydata(ydata)
+            ydata = data['C_raw'][self.ind, :]
+            trace_Craw.set_ydata(ydata)
+            fig.suptitle("Cell {} of {}".format(self.ind, data['C'].shape[0]), x=0.1)
+            plt.draw()
+
+        def check(self, event):
+            check_ind = self.ind # TODO How come checkpoint_ind only saves as 0?
+            data['checkpoint_ind'] = check_ind
+            print(check_ind)
+            plt.close()
+
 
     callback = Index()
 
     # press keep or exclude buttons for each trace
     axkeep = plt.axes([0.05, 0.05, 0.1, 0.075])
     axexclude = plt.axes([0.17, 0.05, 0.1, 0.075])
+    axundo = plt.axes([0.29, 0.05, 0.1, 0.075])
+    axcheck = plt.axes([0.41, 0.05, 0.1, 0.075])
+
     bkeep = Button(axkeep, 'Keep')
     bkeep.on_clicked(callback.keep)
+
     bexclude = Button(axexclude, 'Exclude')
     bexclude.on_clicked(callback.exclude)
+
+    bundo = Button(axundo, 'Undo')
+    bundo.on_clicked(callback.undo)
+
+    bcheck = Button(axcheck, 'Checkpoint')
+    bcheck.on_clicked(callback.check)
+
 
     # alternatively, press k to keep, j to exclude
     def key_press(event):
@@ -100,6 +135,8 @@ def review_traces(data):
             callback.keep(event)
         if event.key == 'j':
             callback.exclude(event)
+        if event.key == 'u':
+            callback.undo(event)
 
     plt.gcf().canvas.mpl_connect('key_press_event', key_press)
 
@@ -110,9 +147,20 @@ def review_traces(data):
 if __name__ == '__main__':
     args = get_args()
     data = sio.loadmat(args.input)
+    fs = args.fs/data['tsub']
 
     if issparse(data['A']):
         data['A'] = np.array(data['A'].todense())
 
-    review_traces(data)
+    print(args.checkpoint)
+    if args.checkpoint:
+        print('fire')
+        checkpoint_ind=data['checkpoint_ind'][0][0]
+        keep = data['keep'][0].tolist()
+    if not args.checkpoint:
+        print('x')
+        checkpoint_ind=0
+        keep = []
+
+    review_traces(data, fs, checkpoint_ind, keep)
     sio.savemat(args.input, data)
